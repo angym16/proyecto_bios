fc653df45c04305cb4a3e9bf1ad107d18cc241b5
Angy Martinez <angiemartinez.sy.678@gmail.com>
2026-05-04 10:01:25 -0500

    Slider (imagenes)

--- js/slider.js
+++ js/slider.js
@@ -1,10 +1,28 @@
+console.log("JS cargado");
 document.addEventListener("DOMContentLoaded", () => {
-  let slides = document.querySelectorAll(".slide");
-  let index = 0;
 
-  setInterval(() => {
-    slides[index].classList.remove("active");
-    index = (index + 1) % slides.length;
-    slides[index].classList.add("active");
-  }, 4000);
+  /* HERO */
+  const heroSlides = document.querySelectorAll('.hero-slider .slide');
+  let heroIndex = 0;
+
+  if (heroSlides.length > 0) {
+    setInterval(() => {
+      heroSlides[heroIndex].classList.remove('active');
+      heroIndex = (heroIndex + 1) % heroSlides.length;
+      heroSlides[heroIndex].classList.add('active');
+    }, 4000);
+  }
+
+  /* SLIDER CARD */
+  const cardSlides = document.querySelectorAll('.slider .slide');
+  let cardIndex = 0;
+
+  if (cardSlides.length > 0) {
+    setInterval(() => {
+      cardSlides[cardIndex].classList.remove('active');
+      cardIndex = (cardIndex + 1) % cardSlides.length;
+      cardSlides[cardIndex].classList.add('active');
+    }, 4000);
+  }
+
 });
